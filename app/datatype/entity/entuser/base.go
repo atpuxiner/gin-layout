cbe8cc2b5d4f5ae65259d4fd21bec38da7a4b596
atpuxiner <atpuxiner@163.com>
2024-02-26 13:49:19 +0800

    chg@datatype

--- app/datatype/entity/entuser/base.go
+++ app/datatype/entity/entuser/base.go
@@ -1,16 +1,20 @@
 package entuser
 
 import (
+	"database/sql"
 	"github.com/atpuxiner/gin-layout/app/datatype/model"
 	"time"
 )
 
 type User struct {
 	model.BaseUser
-	ID        uint      `json:"id"`
-	Name      string    `json:"name"`
-	Age       uint      `json:"age"`
-	Gender    uint      `json:"gender"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint         `json:"id"`
+	Phone     string       `json:"phone"`
+	Password  string       `json:"password"`
+	Name      string       `json:"name"`
+	Age       int          `json:"age"`
+	Gender    int          `json:"gender"`
+	CreatedAt time.Time    `json:"created_at"`
+	UpdatedAt time.Time    `json:"updated_at"`
+	DeletedAt sql.NullTime `json:"deleted_at"`
 }
--- app/datatype/model/user.go
+++ app/datatype/model/user.go
@@ -16,6 +16,6 @@ type User struct {
 	Phone    string `gorm:"column:phone;type:varchar(11);unique_index;not null;comment:手机号"`
 	Password string `gorm:"column:password;type:varchar(255);not null;comment:密码"`
 	Name     string `gorm:"column:name;type:varchar(50);not null;comment:名字"`
-	Age      uint   `gorm:"column:age;type:int;not null;comment:年龄"`
-	Gender   uint   `gorm:"column:gender;type:int;not null;comment:性别(1-男，2-女)"`
+	Age      int    `gorm:"column:age;type:int;not null;comment:年龄"`
+	Gender   int    `gorm:"column:gender;type:int;not null;comment:性别(1-男，2-女)"`
 }
